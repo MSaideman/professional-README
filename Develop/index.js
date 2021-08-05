42df7a83faa125827fce7bba981dbcb6790a7c6a
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-05 15:18:30 -0400

    added questions constant and then statement to make object

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-inquirer 
-.prompt ([
+const question = [
     {
     type: 'input',
     name: 'username',
@@ -61,9 +59,22 @@ inquirer
     name: 'contribution',
     message:'What does the user need to know about contributing to the repo?'
     },
-])
+    ]
+
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        console.log("data", data);
+        let dataObj = data;
+        console.log(dataObj);
+        dataObj = generateMarkdown(dataObj);
+    });
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeToFile('README.md', (fileName, data) => {
+
+})
 
 // TODO: Create a function to initialize app
 function init() {}
